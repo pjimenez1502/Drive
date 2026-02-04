81a4d54761272f77ad32040c42236ae16aa1c38c
pjimenez1502 <pjimenez1502@gmail.com>
2026-02-04 02:19:16 +0100

    Car vol fade in

--- Scene/Car/Car.gd
+++ Scene/Car/Car.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 		print("Agent state: %s par, %s lane, %s manager" % [
 			agent.actor, agent.current_lane, agent.road_manager
 		])
+	fade_car_vol_limit(0)
 
 func _physics_process(delta: float) -> void:
 	update_velocity(delta)
@@ -32,5 +33,13 @@ func update_velocity(delta: float) -> void:
 		return
 	velocity = lerp(velocity, 0.0, delta * 0.2)
 
+func fade_car_vol_limit(limit: float) -> void:
+	var car_vol_tween: Tween = get_tree().create_tween()
+	car_vol_tween.tween_method(set_car_vol_limit, car_volume_limit, limit, 6)
+	
+var car_volume_limit: float = -60
+func set_car_vol_limit(value: float) -> void:
+	car_volume_limit = value
+
 func car_volume() -> void:
-	audio_stream_player_3d.volume_db = -40 + (30 * velocity/max_speed)
+	audio_stream_player_3d.volume_db = clampf(-40 + (30 * velocity/max_speed), -60, car_volume_limit) 
